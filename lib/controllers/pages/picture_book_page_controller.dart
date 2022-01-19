046fc482f05fe2f790f5aec62e70b7f4bc13d99d
j_shin_y <dsisk56gh@gmail.com>
2022-01-20 08:00:15 +0900

    picture_book_pageのcontrollerを作成

---
+++ lib/controllers/pages/picture_book_page_controller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:my_collection/models/src/creature.dart';
+import 'package:my_collection/services/fire_creature_service.dart';
+
+part 'picture_book_page_controller.freezed.dart';
+
+@freezed
+class PictureBookPageState with _$PictureBookPageState {
+  const factory PictureBookPageState({
+    @Default(Creature()) Creature creature,
+    List<Creature>? creatures,
+  }) = _PictureBookPageState;
+}
+
+final pictureBookPageProvider = StateNotifierProvider.autoDispose<
+    PictureBookPageController, PictureBookPageState>(
+  (ref) => PictureBookPageController(),
+);
+
+class PictureBookPageController extends StateNotifier<PictureBookPageState> {
+  PictureBookPageController() : super(const PictureBookPageState()) {
+    _init();
+  }
+
+  PageController? controller;
+  final _fireCreatureService = FireCreatureService();
+
+  void _init() {
+    _fireCreatureService.fetchCreatureList(
+      onValueChanged: (creatures) {
+        state = state.copyWith(creatures: creatures);
+      },
+    );
+  }
+
+  void initialized() {
+    controller = PageController(viewportFraction: 0.6);
+  }
+}
