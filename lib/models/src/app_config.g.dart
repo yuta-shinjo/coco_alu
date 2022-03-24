176912cd29d1d0a1bc9932ee14b74f9c3d60498a
yuta_shin <dsisk56gh@gmail.com>
2022-03-24 19:09:06 +0900

    model作成

---
+++ lib/models/src/app_config.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_config.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_AppConfig _$$_AppConfigFromJson(Map<String, dynamic> json) => _$_AppConfig(
+      minBuildNumberAndroid: json['minBuildNumberAndroid'] as int? ?? 100000,
+      minBuildNumberIos: json['minBuildNumberIos'] as int? ?? 100000,
+    );
+
+Map<String, dynamic> _$$_AppConfigToJson(_$_AppConfig instance) =>
+    <String, dynamic>{
+      'minBuildNumberAndroid': instance.minBuildNumberAndroid,
+      'minBuildNumberIos': instance.minBuildNumberIos,
+    };
